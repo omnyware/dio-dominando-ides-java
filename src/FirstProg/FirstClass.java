4df8a79dd4fe2e8d1fdef30bacff005329010995
omnyware <gtinforserv@gmail.com>
2022-02-19 05:46:00 -0300

    First commit

---
+++ src/FirstProg/FirstClass.java
@@ -0,0 +1,18 @@
+package FirstProg;
+
+import FirstProg.Models.Gatos;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+        Gatos gatos = new Gatos();
+
+
+        System.out.println(gatos);
+
+        /*int a = 3;
+        int b = 2;
+
+        System.out.println("Hello world! " + (a + b));*/
+    }
+}
